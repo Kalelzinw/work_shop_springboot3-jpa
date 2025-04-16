10f0ac017a256ce7a29942d06b4a8932c68d4626
Wendell da Silva Valentim <wendellldasiilva8@gmail.com>
2025-04-16 16:00:59 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -64,6 +64,12 @@ public class OrderItem implements Serializable {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	
+	
+	public Double getSubTotal () {
+		return quantity * price;
+		
+	}
 
 	@Override
 	public int hashCode() {
